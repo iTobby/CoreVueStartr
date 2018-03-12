3bb0a76101ca8556a2665d6b3a33aee10975a4c3
Unknown <tobbyolusanya@gmail.com>
2018-03-12 12:01:52 +0200

    First commit. VueStartr

---
+++ ClientApp/router.js
@@ -0,0 +1,8 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import store from './store'
+import Dashboard from './components/Dashboard.vue'
+import Login from './components/Login.vue'
+
+Vue.use(Router)
+
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 CoreVueStartr
+Build a Secure To-Do App with Vue, ASP.NET Core, and Okta
+https://scotch.io/tutorials/build-a-secure-to-do-app-with-vuejs-aspnet-core-and-okta
